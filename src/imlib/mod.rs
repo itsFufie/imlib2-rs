14351a27d426e2ff1473e45be2b0cc89bae98202
its_fufie <fufcore@gmail.com>
2025-02-02 20:00:42 -0300

    making load_image safe to call

--- src/imlib/mod.rs
+++ src/imlib/mod.rs
@@ -1,10 +1,10 @@
 use std::ffi::CString;
 
-use crate::{imlib_load_image, Imlib_Image};
+use crate::{ imlib_load_image, Imlib_Image };
 
-
-
-pub unsafe fn load_image(path: &str) -> Imlib_Image {
-    let image_path_c_str = CString::new(path).unwrap();
-    imlib_load_image(image_path_c_str.as_ptr()) 
+pub fn load_image(path: &str) -> Imlib_Image {
+    unsafe {
+        let image_path_c_str = CString::new(path).unwrap();
+        imlib_load_image(image_path_c_str.as_ptr())
+    }
 }
